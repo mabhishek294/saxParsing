e2a37746db82ff056067c88e0a433ab19b0f0d64
abhishek <mabhishek294@gmail.com>
2017-12-28 11:06:23 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abhishek
+ */
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Sax parser");
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+    try {
+        SAXParser saxParser = saxParserFactory.newSAXParser();
+        MyHandler handler = new MyHandler();
+        saxParser.parse(new File("Documents/sample.xml"), handler);
+        List<Employee> empList = handler.getEmpList();
+        for(Employee emp : empList)
+            System.out.println(emp);
+    } catch (ParserConfigurationException | SAXException | IOException e) {
+        e.printStackTrace();
+    }
+    
+        
+        
+    }
+}
